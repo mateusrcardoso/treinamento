ee4340486fcb60cf453c3ff64cd2e1909761dc46
mateusrcardoso <mateusrcardoso@gmail.com>
2021-06-01 22:22:55 -0300

    Criação do controller e do model do produto

---
+++ backend/Controllers/ProdutoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutoController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ProdutoController> _logger;
+
+        public ProdutoController(ILogger<ProdutoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Produto> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Produto
+            {
+                id = index,
+                nome = "Produto " + index.ToString(),
+                descricao = "Descrição do Produto " + index.ToString(),
+                valor = 100 + index
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ backend/Models/Produto.cs
@@ -0,0 +1,10 @@
+namespace backend.Models
+{
+    public class Produto
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string descricao { get; set; }
+        public decimal valor { get; set; }
+    }
+}
